9185a2227c03b9895a7a9367698ddd02fbf0a5b8
Michael John <michael.john@gmx.at>
2023-06-19 07:08:45 +0200

    DB changes

--- thegadget/resources/database/TheGadget.sql
+++ thegadget/resources/database/TheGadget.sql
@@ -86,3 +86,112 @@ INSERT INTO Dates (event_date, description) VALUES ('1945-08-14', 'Japanese gove
 INSERT INTO Dates (event_date, description) VALUES ('1945-08-15', 'Japan surrenders (V-J Day)');
 INSERT INTO Dates (event_date, description) VALUES ('1945-10-24', 'United Nations officially established');
 
+select event_date, STRFTIME("%Y", event_date) year, STRFTIME("%W", event_date) + 1 week, count(*) events
+from dates 
+group by STRFTIME("%Y", event_date), STRFTIME("%W", event_date)
+
+select STRFTIME("%Y", event_date) year, count(event_date) events
+from dates
+group by STRFTIME("%Y", event_date)
+
+select event_date, description, length(description), length(desc_long) 
+from dates
+order by event_date 
+
+select description, count(description) from dates
+group by description 
+order by description 
+
+select STRFTIME("%W", "1947-01-01") + 1 week
+
+select p.name 
+from dates d, people p
+where p.name  in 
+(select d.desc_long from dates d) 
+
+select p.name, p.join_date, d.description 
+from dates d, people p
+where p.join_date = d.event_date 
+
+select cx, cy, round(cx, 2), round(cy, 2) from places
+
+update places set cx = round(cx, 2), cy = round(cy, 2)
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Klaus Fuchs', 'aKlaus Fuchs was a German-born physicist who worked on the Manhattan Project and provided classified information about atomic weapons to the Soviet Union.', '1911-12-29');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Julius Rosenberg', 'Julius Rosenberg was an American electrical engineer and a key figure in the Soviet atomic espionage network. He and his wife, Ethel Rosenberg, were involved in transmitting classified information about the atomic bomb to the Soviet Union.', '1918-05-12');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Ethel Rosenberg', 'Ethel Rosenberg was an American activist who, along with her husband, Julius Rosenberg, participated in transmitting classified information about the atomic bomb to the Soviet Union.', '1915-09-28');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Theodore Hall', 'Theodore Hall was an American physicist who worked on the Manhattan Project and passed classified information about the atomic bomb to the Soviet Union. He was the youngest scientist to work on the project.', '1925-10-20');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('David Greenglass', 'David Greenglass was an American machinist and former soldier who worked on the Manhattan Project. He provided classified information about atomic bomb designs to his brother-in-law, Julius Rosenberg, who passed it to the Soviet Union.', '1922-03-02');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Morris Cohen', 'Morris Cohen, also known as "Agent Lattimore," was an American spy who worked as a courier for Soviet intelligence.', NULL);
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Harry Gold', 'Harry Gold was an American laboratory chemist who acted as a courier for Soviet atomic spies, including passing information about the Manhattan Project to the Soviet Union.', '1910-12-11');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('George Koval', 'George Koval was a Soviet intelligence officer and American-born physicist who infiltrated the Manhattan Project and provided valuable information to the Soviet Union.', '1913-12-25');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Irving Lerner', 'Irving Lerner was an American film director and screenwriter. There is no evidence suggesting his involvement in atomic espionage.', NULL);
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Alan Nunn May', 'Alan Nunn May was a British physicist who worked on the Manhattan Project and provided classified information to the Soviet Union.', '1911-05-24');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Saville Sax', 'Saville Sax was an American chemist who worked on the Manhattan Project and was involved in espionage activities for the Soviet Union.', NULL);
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Oscar Seborer', 'Oscar Seborer was an American electrical engineer who participated in Soviet atomic espionage during the Manhattan Project.', NULL);
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Morton Sobell', 'Morton Sobell was an American engineer who was involved in passing atomic-related information to the Soviet Union during the Manhattan Project.', '1917-04-11');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Melita Norwood', 'Melita Norwood was a British secretary who, during World War II, passed atomic secrets to the Soviet Union. She continued her espionage activities even after the war.', '1912-03-25');
+
+INSERT INTO people (name, description, birth_date)
+VALUES ('Arthur Adams', 'Arthur Adams was an American machinist who worked on the Manhattan Project and later provided classified information to the Soviet Union.', NULL);
+
+CREATE TABLE Buildings (
+  building_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  place_id INT,
+  FOREIGN KEY (place_id) REFERENCES Places(place_id)
+);
+
+ALTER TABLE buildings
+ADD coords_polygon polygon;
+
+ALTER TABLE buildings
+ADD coords_text TEXT;
+
+SELECT load_extension("mod_spatialite.so")
+
+SELECT load_extension("mod_spatialite");
+
+select spatialite_version()
+
+select spatialite_target_cpu()
+
+select geos_version()
+
+select HasGeoCallbacks()
+
+select AsWKT(ST_LineFromText(b.coords_text)) from Buildings b  
+
+select AsWKT(b.coords_text) from Buildings b  
+
+select AsText(Envelope(b.coords_polygon)) from Buildings b  
+
+select b.building_id, ST_AsText(coords_text), GeometryType(b.coords_polygon) from Buildings b 
+
